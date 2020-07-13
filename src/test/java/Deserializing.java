fac33ce00d47914b1e4d2478ca0b49960404e92b
Иван <ivan.jojojos@yandex.ru>
2020-07-14 01:39:47 +0400

    True entities and working test

--- src/test/java/Deserializing.java
+++ src/test/java/Deserializing.java
@@ -1,39 +1,12 @@
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import entities.Example;
-import entities.Fact;
-import entities.FactList;
-import entities.User;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static io.restassured.RestAssured.given;
+import requests.RequestApi;
 
 public class Deserializing {
 
     @Test()
-    public void deserialize() throws JsonProcessingException {
-        Fact fact =
-                given().
-                        when().
-                        get("https://cat-fact.herokuapp.com/facts/").
-                        as(Fact.class);
-        Assert.assertEquals(
-                "Kasimir Schulz",
-        fact.getFactList().get(0).getUser().getName().toString()
-        );
-        Map<User, Long> factsByUser = fact.getFactList().stream().collect(Collectors.groupingBy(Fact::getUser, Collectors.counting()));
-
-        for(Map.Entry<User, Long> item : factsByUser.entrySet()) {
-            System.out.println(item.getKey().toString() + " - " + item.getValue());
-        }
+    public void deserialize() {
+            Assert.assertEquals("Kasimir Shulz",
+                    RequestApi.maxEntry().getName());
     }
 }
--- src/test/java/requests/RequestApi.java
+++ src/test/java/requests/RequestApi.java
@@ -1,14 +1,27 @@
 package requests;
 
-import entities.Example;
+import entities.Fact;
+import entities.User;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 
 public class RequestApi {
-    public RequestApi() {
-        given().
-                when().
-                get("https://cat-fact.herokuapp.com/facts/").
-                as(Example .class);
+    public static User maxEntry() {
+        Fact fact =
+                given().
+                        when().
+                        get("https://cat-fact.herokuapp.com/facts/").
+                        as(Fact.class);
+
+        Map<User, Long> factsCountByUser = fact.getFactList().
+                stream().
+                filter(e -> e.getUser() != null).
+                collect(Collectors.groupingBy(Fact::getUser, Collectors.counting()));
+
+        return Collections.max(factsCountByUser.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 }
